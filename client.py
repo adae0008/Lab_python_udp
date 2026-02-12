bf65c082f31a192d8cf2e4074fd17523603d57b3
Ekoue Jean Adama <adae0008@uqar.ca>
2026-02-11 22:03:37 -0500

    partie 4 sha256

--- client.py
+++ client.py
@@ -1,37 +1,20 @@
-from pathlib import Path
-
-p = Path("data/message.txt")
-
-s = p.read_text(encoding="utf-8")
-
-print(type(s), len(s))
-print(s)
-
-b = p.read_bytes()
-
-print(type(b), len(b))
-print(b[:20])
-
-
-b2 = s.encode("utf-8")
-s2 = b.decode("utf-8")
-
-print(len(b2), type(b2))
-print(type(s2))
-
-
-
-
 import socket
+import hashlib
 from pathlib import Path
 
 HOST = "127.0.0.1"
-PORT = 12345
+PORT = 12346
 
+#Ici c'est pour lire message
 msg = Path("data/message.txt").read_text(encoding="utf-8").encode("utf-8")
 
+h = hashlib.sha256(msg).hexdigest()
+
+# Créer payload : message + séparateur + hash
+payload = msg + b"\x00" + h.encode("ascii")
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.sendto(msg, (HOST, PORT))
+    s.sendto(payload, (HOST, PORT))
 
     data, _ = s.recvfrom(1024)
-    print("Réponse du serveur :", data.decode("utf-8"))
+    print("Réponse serveur :", data.decode("utf-8"))
